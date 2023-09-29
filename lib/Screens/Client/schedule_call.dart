659ccfc8ebef71cf9399d00cda0167918e22f8c0
Huzafa <huzaifatawab33@gmail.com>
2023-09-29 16:13:35 +0500

    schedule

--- lib/Screens/Client/schedule_call.dart
+++ lib/Screens/Client/schedule_call.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:booking_calendar/booking_calendar.dart';
 
@@ -11,23 +12,39 @@ class ScheduleACall extends StatefulWidget {
 
 class _ScheduleACallState extends State<ScheduleACall> {
   final now = DateTime.now();
-  late BookingService callServiceBooking;
+  BookingService? callServiceBooking;
 
   @override
   void initState() {
     super.initState();
-    // DateTime.now().startOfDay
-    // DateTime.now().endOfDay
-    callServiceBooking = BookingService(
-        serviceName: 'Call Service',
-        serviceDuration: 60,
-        bookingEnd: DateTime(now.year, now.month, now.day, 18, 0),
-        bookingStart: DateTime(now.year, now.month, now.day, 8, 0));
+
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      FirebaseFirestore.instance
+          .collection('userDetails')
+          .doc(user.uid)
+          .get()
+          .then((doc) {
+        if (doc.data() != null) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          callServiceBooking = BookingService(
+              serviceName: 'Call Service',
+              serviceDuration: 60,
+              userId: user.uid,
+              userEmail: user.email,
+              userPhoneNumber: data["phoneNumber"],
+              userName: data["name"].toString(),
+              bookingEnd: DateTime(now.year, now.month, now.day, 18, 0),
+              bookingStart: DateTime(now.year, now.month, now.day, 8, 0));
+        }
+        setState(() {});
+      });
+    }
   }
 
   Stream<dynamic>? getBookingStreamMock(
       {required DateTime end, required DateTime start}) {
-    return Stream.value([]);
+    return FirebaseFirestore.instance.collection("callBookings").snapshots();
   }
 
   Future<dynamic> uploadBookingMock(
@@ -46,24 +63,32 @@ class _ScheduleACallState extends State<ScheduleACall> {
     ///here you can parse the streamresult and convert to [List<DateTimeRange>]
     ///take care this is only mock, so if you add today as disabledDays it will still be visible on the first load
     ///disabledDays will properly work with real data
-    DateTime first = now;
-    DateTime tomorrow = now.add(Duration(days: 1));
-    DateTime second = now.add(const Duration(minutes: 55));
-    DateTime third = now.subtract(const Duration(minutes: 240));
-    DateTime fourth = now.subtract(const Duration(minutes: 500));
-    converted.add(
-        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
-    converted.add(DateTimeRange(
-        start: second, end: second.add(const Duration(minutes: 23))));
-    converted.add(DateTimeRange(
-        start: third, end: third.add(const Duration(minutes: 15))));
-    converted.add(DateTimeRange(
-        start: fourth, end: fourth.add(const Duration(minutes: 50))));
+    // DateTime first = now;
+    // DateTime tomorrow = now.add(const Duration(days: 1));
+    // DateTime second = now.add(const Duration(minutes: 55));
+    // DateTime third = now.subtract(const Duration(minutes: 240));
+    // DateTime fourth = now.subtract(const Duration(minutes: 500));
+    // converted.add(
+    //     DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
+    // converted.add(DateTimeRange(
+    //     start: second, end: second.add(const Duration(minutes: 23))));
+    // converted.add(DateTimeRange(
+    //     start: third, end: third.add(const Duration(minutes: 15))));
+    // converted.add(DateTimeRange(
+    //     start: fourth, end: fourth.add(const Duration(minutes: 50))));
 
-    //book whole day example
-    converted.add(DateTimeRange(
-        start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
-        end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
+    // //book whole day example
+    // converted.add(DateTimeRange(
+    //     start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
+    //     end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
+    List<DateTimeRange> converted = [];
+    for (var i = 0; i < streamResult.size; i++) {
+      Map<String, dynamic> item =
+          streamResult.docs[i].data() as Map<String, dynamic>;
+      converted.add(DateTimeRange(
+          start: (DateTime.parse(item["bookingStart"])),
+          end: (DateTime.parse(item["bookingEnd"]))));
+    }
     return converted;
   }
 
@@ -81,23 +106,25 @@ class _ScheduleACallState extends State<ScheduleACall> {
       appBar: AppBar(
         title: const Text('Booking Calendar Demo'),
       ),
-      body: Center(
-        child: BookingCalendar(
-          bookingService: callServiceBooking,
-          convertStreamResultToDateTimeRanges: convertStreamResultMock,
-          getBookingStream: getBookingStreamMock,
-          uploadBooking: uploadBookingMock,
-          loadingWidget: const Text('Fetching data...'),
-          uploadingWidget: const CircularProgressIndicator(),
-          locale: 'en_EN',
-          startingDayOfWeek: StartingDayOfWeek.monday,
+      body: callServiceBooking != null
+          ? Center(
+              child: BookingCalendar(
+                bookingService: callServiceBooking!,
+                convertStreamResultToDateTimeRanges: convertStreamResultMock,
+                getBookingStream: getBookingStreamMock,
+                uploadBooking: uploadBookingMock,
+                loadingWidget: const Text('Fetching data...'),
+                uploadingWidget: const CircularProgressIndicator(),
+                locale: 'en_EN',
+                startingDayOfWeek: StartingDayOfWeek.monday,
 
-          wholeDayIsBookedWidget:
-              const Text('Sorry, for this day everything is booked'),
-          //disabledDates: [DateTime(2023, 1, 20)],
-          //disabledDays: [6, 7],
-        ),
-      ),
+                wholeDayIsBookedWidget:
+                    const Text('Sorry, for this day everything is booked'),
+                //disabledDates: [DateTime(2023, 1, 20)],
+                disabledDays: const [6, 7],
+              ),
+            )
+          : const SizedBox(),
     );
   }
 }
