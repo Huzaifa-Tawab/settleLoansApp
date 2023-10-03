9e34a75ac32e5c344f92e6d40e3f2bc8a1a4c9ce
umerfarooq-lodhi <141773987+umerfarooq-lodhi@users.noreply.github.com>
2023-10-03 17:48:16 +0500

    new

--- lib/Components/Popup.dart
+++ lib/Components/Popup.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:settle_loans/Constrains/colors.dart';
 
-class Popup extends StatefulWidget {
+class Popup extends StatelessWidget {
   const Popup({super.key});
 
   @override
-  State<Popup> createState() => _PopupState();
-}
-
-class _PopupState extends State<Popup> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 100, vertical: 300),
-        color: PrimaryColor(),
-        height: 280,
-        width: 265,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/CheckCircle.png'),
-            // SizedBox(
-            //   height: 20,
-            // ),
-            Text(
-                'Thank you for booking\n you will hear from our\n execuitive shortly'),
-          ],
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 4,
+            offset: Offset(4, 8), // Shadow position
+          ),
+        ],
+      ),
+      margin: EdgeInsets.symmetric(horizontal: 80, vertical: 250),
+      height: 400,
+      width: 300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/CheckCircle.png'),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(
+            endIndent: 20,
+            indent: 20,
+          ),
+          Text(
+              'Thank you for booking\n you will hear from our\n execuitive shortly'),
+        ],
       ),
     );
   }
